285e3a7fc853b8d54198cea4dbebb740988d8bc2
WahomeKennedy <kennedywahome.24@gmail.com>
2023-08-24 00:25:52 +0300

    Initial API commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+#from flask_restful import Api, Resource
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -7,6 +8,7 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    #api = Api(app)
 
     if test_config is None:
         #load the instance config, if it exists, when not testing
